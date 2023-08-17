9973700e3e730eeb45005a7713b21beebdddac04
DeMo55 <deyaam10@gmail.com>
2023-08-16 20:01:01 -0700

    22rd update for 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,32 +1,29 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- * positive_or_negative - determine if a number is positive, negative or zero.
+ * main - Determines if a number is positive, negative or zero.
  *
- * Description: Check if positive or negative
- *
- * @i: describe input
- *
+ * Return: Always 0 (Success)
  */
-
-void positive_or_negative(int i)
+int main(void)
 {
-	/* i is the received number to check*/
-	int n = i;
+	int n;
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-{
-	printf(“%d is positive\n”, n);
-}
-else if (i == 0)
-{
-	printf(“%d is zero\n”, n);
-}
-else
-{
-	printf(“%d is negative\n”, n);
-}
-
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
